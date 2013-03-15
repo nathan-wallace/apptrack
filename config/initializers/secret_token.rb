28f74f93070e05eeadee923401b10b51832d738b
Nathan Wallace <nathan.s.wallace@gmail.com>
2013-03-15 14:27:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apptrack::Application.config.secret_token = '7663dff3b2a6fcc48d66f9515e2a7c921416c0c17dba56e4745f3c07491b71f65d1eb97f15853b27158e0464dc0dcee953e0d6254fd4f79ad661ad8bc33d0c3d'
